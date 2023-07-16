cb3257640a1339dec86a353514f54df700bc0727
its-sarath28 <sarathrs220@gmail.com>
2023-07-16 16:54:31 +0530

    final

---
+++ app/components/sidebar/MobileItem.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+import clsx from "clsx";
+
+interface MobileItemProps {
+  href: string;
+  icon: any;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const MobileItem: React.FC<MobileItemProps> = ({ 
+  href, 
+  icon: Icon, 
+  active,
+  onClick
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      return onClick();
+    }
+  };
+
+  return ( 
+    <Link 
+      onClick={handleClick} 
+      href={href} 
+      className={clsx(`
+        group 
+        flex 
+        gap-x-3 
+        text-sm 
+        leading-6 
+        font-semibold 
+        w-full 
+        justify-center 
+        p-4 
+        text-sky-500 
+        hover:text-white 
+        hover:bg-sky-500
+      `,
+        active && 'bg-sky-500 text-white'
+      )}>
+      <Icon className="h-6 w-6" />
+    </Link>
+   );
+}
+ 
+export default MobileItem;
