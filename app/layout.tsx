eb7316d81651bda242125d63e5bac7cd3621913a
Sarath R S <78993686+its-sarath28@users.noreply.github.com>
2023-07-30 22:55:13 +0530

    Update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import ActiveStatus from './components/ActiveStatus'
 import ToasterContext from './context/ToasterContext'
 
 export const metadata = {
-  title: 'Chat App',
+  title: 'Conversa',
   description: 'Chat App by Sarath',
 }
 
